aae289053b80c6c981b6db8fd20505f3dc371c7c
张瑞兰 <zhangruilan007@gmail.com>
2017-06-26 16:29:35 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,17 @@ resources :products do
     end
 
    resources :cart_items
-   resources :orders
 
    namespace :account do
-     resources :orders
-   end
+   resources :orders
+ end
+
+     resources :orders do
+       member do
+        post :pay_with_alipay
+        post :pay_with_wechat
+      end
+     end
+
+
 end
